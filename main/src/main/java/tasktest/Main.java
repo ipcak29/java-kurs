13fbd59b313a65990e89cb48d3bc4821d4681f93
Kacper <ipcak29@gmail.com>
2026-09-22 15:12:08 +0200

    feature: hello anna

--- main/src/main/java/tasktest/Main.java
+++ main/src/main/java/tasktest/Main.java
@@ -2,6 +2,6 @@ package tasktest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello johny");
+        System.out.println("Hello anna");
     }
 }
