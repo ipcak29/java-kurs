0e139130201824ed9cf2c57e71b388fb9ba807fa
Kacper <ipcak29@gmail.com>
2026-09-22 15:14:18 +0200

    feature: hello wiesio

--- main/src/main/java/tasktest/Main.java
+++ main/src/main/java/tasktest/Main.java
@@ -2,6 +2,6 @@ package tasktest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello anna");
+        System.out.println("Hello wiesio");
     }
 }
