cc78231f5cb61679907b0c4aa2cbdfabaab196df
Kacper <ipcak29@gmail.com>
2026-09-23 16:09:10 +0200

    innit: hello anna

--- main/src/main/java/org/example/Main.java
+++ main/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, anna and ola");
+        System.out.println("Hello, anna");
     }
 }
